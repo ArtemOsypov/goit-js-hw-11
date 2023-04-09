efc138f4f4d89a6fa44bcf29994f505ff685e731
ArtemOsypov <artem.osypov@gmail.com>
2023-04-09 13:27:32 +0300

    lightbox add/import markup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import ImagesApiLoad from './js/fetch-image';
+import { createGalleryMarkup } from './js/gallary-markup.js';
+import ImagesApiLoad from './js/fetch-image.js';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   galleryContainer: document.querySelector('.gallery'),
@@ -10,8 +13,9 @@ const refs = {
 
 refs.input.addEventListener('submit', onSearch);
 refs.btnLoadMore.addEventListener('click', onLoadMore);
-refs.btnLoadMore.disabled = true;
-refs.btnLoadMore.classList.add('is-hidden');
+loadMoreBtnDisabled();
+
+let lightbox = 0;
 const imagesApiLoad = new ImagesApiLoad();
 
 function onSearch(e) {
@@ -34,16 +38,18 @@ function onSearch(e) {
         return Notiflix.Notify.failure('Ничего не найдено, повторите запрос!');
       }
       Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
-      // console.log(`найдено ${totalHits} картинок`);
-      // console.log(hits);
-      // console.log(totalHits);
-      // console.log(hits.length);
       imagesApiLoad.loadedHits = imagesApiLoad.per_page;
-      createGalleryMarkup(hits);
-      refs.btnLoadMore.disabled = false;
-      refs.btnLoadMore.classList.remove('is-hidden');
-
-      //   return response.data;
+      refs.galleryContainer.insertAdjacentHTML(
+        'beforeend',
+        createGalleryMarkup(hits)
+      );
+      loadMoreBtnactive();
+      lightbox = new SimpleLightbox('.gallery  a', {
+        captionDelay: 250,
+        scrollZoom: false,
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+      });
     })
     .catch();
 }
@@ -60,92 +66,31 @@ function onLoadMore() {
         console.log(totalHits);
         console.log(imagesApiLoad.per_page);
         console.log(imagesApiLoad.page);
-        refs.btnLoadMore.disabled = true;
+        loadMoreBtnDisabled();
         return Notiflix.Notify.info(
           'We are sorry, but you have reached the end of search results.'
         );
       }
       imagesApiLoad.incrementLoadedHits(hits);
-      console.log(hits);
-      console.log(totalHits);
-      console.log(imagesApiLoad.per_page);
-      console.log(imagesApiLoad.page);
 
-      createGalleryMarkup(hits);
-
-      //   return response.data;
+      refs.galleryContainer.insertAdjacentHTML(
+        'beforeend',
+        createGalleryMarkup(hits)
+      );
     })
     .catch();
 }
 
-// async function fetchImages(searchQuery) {
-//   const searchParams = new URLSearchParams({
-//     key: API_KEY,
-//     q: searchQuery,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: 'true',
-//     page: 1,
-//     per_page: 40,
-//   });
-//   const url = `${BASE_URL}?${searchParams}`;
-//   const response = await axios.get(url);
-//   //   this.incrementPage();
-
-//   return response.data;
-// }
-
-function createGalleryMarkup(images) {
-  const markup = images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-    <div class="photo-card">
-      <a href="${webformatURL}">
-        <img
-          class="photo-card__img"
-          src="${largeImageURL}" 
-          alt="${tags}" 
-          loading="lazy" 
-          width="320"
-          height="212"
-        />
-      </a>
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>
-          <span>${likes}</span>
-        </p>
-        <p class="info-item">
-          <b>Views</b>
-          <span>${views}</span>
-        </p>
-        <p class="info-item">
-          <b>Comments</b>
-          <span>${comments}</span>
-        </p>
-        <p class="info-item">
-          <b>Downloads</b>
-          <span>${downloads}</span>
-        </p>
-      </div>
-    </div>
-    `;
-      }
-    )
-    .join('');
-
-  refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
-}
-
 function clearGelleryContainer() {
   refs.galleryContainer.innerHTML = '';
+}
+
+function loadMoreBtnDisabled() {
+  refs.btnLoadMore.disabled = true;
+  refs.btnLoadMore.classList.add('is-hidden');
+}
+
+function loadMoreBtnactive() {
+  refs.btnLoadMore.disabled = false;
+  refs.btnLoadMore.classList.remove('is-hidden');
 }
